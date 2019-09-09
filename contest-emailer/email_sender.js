6b8cf28a22bac959bbeb511c5726d975ac85180c
KnightNiwrem <merwinwee@gmail.com>
2019-09-09 09:12:51 +0800

    Fix auth constant naming

--- contest-emailer/email_sender.js
+++ contest-emailer/email_sender.js
@@ -7,8 +7,8 @@ const nodemailer = require('nodemailer');
 //--------------
 // Configuration
 //--------------
-const username = process.env.AWS_ACCESS_KEY_ID;
-const password = process.env.AWS_SECRET_ACCESS_KEY;
+const user = process.env.AWS_ACCESS_KEY_ID;
+const pass = process.env.AWS_SECRET_ACCESS_KEY;
 
 const isTesting = !!process.env.IS_TESTING;
 const smtpHost = 'email-smtp.us-west-2.amazonaws.com';
